989b8033facf684f98ab41af256063be50446c04
David Cotter <daveguy28@gmail.com>
2024-02-08 08:54:06 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,8 @@ function rungame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2); 
+    } else if (gameType === "multiply") {
+        displayMultipyQuestion(num1, num2);
     } else {
         alert('Unknown game type: ${gameType}');
         throw 'Unknown game type: ${gameType}, Aborting!';
@@ -70,6 +72,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert('Unimplemented operator ${operator}');
         throw 'Unimplemented operator ${operator}, Aborting!';
@@ -108,6 +112,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultipyQuestion() {
-
+function displayMultipyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
