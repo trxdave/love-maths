aba8f5ea5fd3f512fca15715eed1a0b31146dee4
David Cotter <daveguy28@gmail.com>
2024-02-08 09:09:05 +0000

    Add code for subraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,8 @@ function rungame(gameType) {
         displayAdditionQuestion(num1, num2); 
     } else if (gameType === "multiply") {
         displayMultipyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert('Unknown game type: ${gameType}');
         throw 'Unknown game type: ${gameType}, Aborting!';
@@ -74,6 +76,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert('Unimplemented operator ${operator}');
         throw 'Unimplemented operator ${operator}, Aborting!';
@@ -108,8 +112,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultipyQuestion(operand1, operand2) {
